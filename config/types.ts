d72888bc7f286fcb36624a3380a304990fd40543
dmitriy <lmidorunl@gmail.com>
2023-04-19 19:19:31 +0400

    refactor: update webpack config types

--- config/types.ts
+++ config/types.ts
@@ -1,4 +1,20 @@
-export type Env= {
-  mode: 'production' | 'development'
-  port?: number
+export type BuildMode = 'production' | 'development'
+
+export type BuildPaths = {
+  entry: string
+  build: string
+  html: string
+  src: string
+}
+
+export type BuildEnv = Partial<{
+  mode: BuildMode
+  port: number
+}>
+
+export type BuildOptions = {
+  mode: BuildMode
+  paths: BuildPaths
+  isDev: boolean
+  port: number
 }
