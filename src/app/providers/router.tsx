809324357d3f7a5b3b476eb33384655bea381d5f
dmitriy <lmidorunl@gmail.com>
2023-04-20 16:00:13 +0400

    refactor: remove comments

--- src/app/providers/router.tsx
+++ src/app/providers/router.tsx
@@ -1,6 +1,4 @@
 import App from 'app/index'
-// import About from 'pages/About'
-// import Home from 'pages/Home'
 
 import React, { lazy } from 'react'
 import { createBrowserRouter } from 'react-router-dom'
