26a3551f9a39c5b57e2135600f36da7734d52e8a
dmitriy <lmidorunl@gmail.com>
2023-04-29 21:26:20 +0400

    refactor: move cn util

--- src/shared/lib/cn/cn.test.ts
+++ src/shared/lib/cn/cn.test.ts
@@ -1,4 +1,4 @@
-import { cn } from './cn'
+import { cn } from './index'
 
 describe('cn', () => {
   test('test', () => {
--- src/shared/lib/cn/index.ts
+++ src/shared/lib/cn/index.ts
@@ -1,1 +1,18 @@
-export { cn } from './cn'
+type ModsType = Record<string, string | boolean>
+
+export const cn = (
+  className: string,
+  mods: ModsType = {},
+  additional?: Array<string | undefined>
+) => {
+  // eslint-disable-next-line array-callback-return
+  const filteredMods = Object.entries(mods).map((entry) => {
+    const [key, value] = entry
+
+    if (value) {
+      return key
+    }
+  })
+
+  return [className, ...filteredMods, additional?.join(' ')].join(' ').trim()
+}
