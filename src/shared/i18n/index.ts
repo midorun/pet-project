8c16082be3b2dd0c81be35fab2bac66f41dff1cc
dmitriy <lmidorunl@gmail.com>
2023-04-29 21:54:43 +0400

    refactor: update i18n config

--- src/shared/i18n/index.ts
+++ src/shared/i18n/index.ts
@@ -16,7 +16,7 @@ void i18n
     // or even better, manage them via a UI: https://react.i18next.com/guides/multiple-translation-files#manage-your-translations-with-a-management-gui)
 
     fallbackLng: false,
-    debug: __IS_DEV__,
+    debug: false,
   })
 
 export default i18n
